c3b3d1b092e0d6c9dd571d1e5ffe03798bee3814
nuttle <allen.nuttle@gmail.com>
2022-03-22 21:06:58 -0400

    Non-drive subsystems need work

--- src/main/cpp/subsystems/FeederSubsystem.cpp
+++ src/main/cpp/subsystems/FeederSubsystem.cpp
@@ -15,21 +15,31 @@ FeederSubsystem::FeederSubsystem() noexcept
         {"kSmartCurrentFreeLimit", uint{10}},  // Amps
     };
 
+    const SmartMotorBase::ConfigMap climberConfig = {
+        {"kStatus1", uint{250}},
+        {"kStatus2", uint{250}},
+        {"kRampRate", double{0.5}},
+        {"kLimitSwitchFwdPolarity", uint{0}}, // 0 = Normally Open, 1 = Normally Closed
+        {"kLimitSwitchRevPolarity", uint{0}},
+    };
+
     m_intakeMotor = SparkMaxFactory::CreateSparkMax("Intake", 9, false);
     m_elevatorMotor = SparkMaxFactory::CreateSparkMax("Elevator", 10, true);
     m_feederMotor = SparkMaxFactory::CreateSparkMax("Feeder", 11, true);
     m_climberMotor = SparkMaxFactory::CreateSparkMax("Climber", 12, false);
 
     m_intakeRaise = std::make_unique<frc::DoubleSolenoid>(1, frc::PneumaticsModuleType::REVPH, 0, 1);
-    m_intakeRelease = std::make_unique<frc::DoubleSolenoid>(1, frc::PneumaticsModuleType::REVPH, 2, 3);
+    m_intakeRelease = std::make_unique<frc::DoubleSolenoid>(1, frc::PneumaticsModuleType::REVPH, 13, 12);
 
     m_intakeMotor->SetConfig(config);
     m_elevatorMotor->SetConfig(config);
     m_feederMotor->SetConfig(config);
+    m_climberMotor->SetConfig(climberConfig);
 
     m_intakeMotor->ApplyConfig(false);
     m_elevatorMotor->ApplyConfig(false);
     m_feederMotor->ApplyConfig(false);
+    m_climberMotor->ApplyConfig(false);
 
     Pneumatics();
 }
@@ -40,31 +50,6 @@ void FeederSubsystem::Periodic() noexcept
     m_elevatorMotor->Periodic();
     m_feederMotor->Periodic();
     m_climberMotor->Periodic();
-}
-
-void FeederSubsystem::Set(double percent) noexcept
-{
-    m_elevatorMotor->SetVoltage(percent * 12_V);
-    m_intakeMotor->SetVoltage(percent * 12_V);
-
-    if (percent < 0.0)
-    {
-        m_feederMotor->SetVoltage(percent * 12_V);
-    }
-    else
-    {
-        m_feederMotor->SetVoltage(0.0_V);
-    }
-}
-
-void FeederSubsystem::Hold() noexcept
-{
-    m_feederMotor->Stop();
-}
-
-void FeederSubsystem::Fire() noexcept
-{
-    m_feederMotor->SetVoltage(12_V);
 }
 
 void FeederSubsystem::Pneumatics() noexcept
@@ -93,10 +78,37 @@ void FeederSubsystem::LowerIntake() noexcept
     m_intakeRaise->Set(frc::DoubleSolenoid::Value::kReverse);
 }
 
+void FeederSubsystem::Set(double percent) noexcept
+{
+    m_elevatorMotor->SetVoltage(percent * 12_V);
+    m_intakeMotor->SetVoltage(percent * 12_V);
+
+    if (percent < 0.0)
+    {
+        m_feederMotor->SetVoltage(percent * 12_V);
+    }
+    else
+    {
+        m_feederMotor->Stop();
+    }
+}
+
+void FeederSubsystem::Hold() noexcept
+{
+    m_feederMotor->Stop();
+}
+
+void FeederSubsystem::Fire() noexcept
+{
+    m_feederMotor->SetVoltage(12_V);
+}
+
 void FeederSubsystem::Raise() noexcept
 {
+    //    m_climberMotor->SetVoltage(2.0_V);
 }
 
 void FeederSubsystem::Lower() noexcept
 {
+    //    m_climberMotor->SetVoltage(-2.0_V);
 }
