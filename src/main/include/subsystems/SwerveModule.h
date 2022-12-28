88b8ea642d2551cef06263834278cd5da5512089
nuttle <allen.nuttle@gmail.com>
2022-12-27 20:35:50 -0500

    Prepare for 2023

--- src/main/include/subsystems/SwerveModule.h
+++ src/main/include/subsystems/SwerveModule.h
@@ -7,6 +7,7 @@
 #include "subsystems/SparkMax.h"
 
 #include <frc/controller/ProfiledPIDController.h>
+// XXX  #include <frc/kinematics/SwerveModulePosition.h>
 #include <frc/kinematics/SwerveModuleState.h>
 #include <frc/shuffleboard/ComplexWidget.h>
 #include <frc/shuffleboard/SimpleWidget.h>
@@ -209,6 +210,7 @@ public:
   // Commands which combine the above functions, useful for teleop driving.
   void ResetEncoders() noexcept;
   const frc::SwerveModuleState GetState() noexcept;
+  // XXX  const frc::SwerveModulePosition GetPosition() noexcept;
   void SetDesiredState(const frc::SwerveModuleState &state) noexcept;
 
   // Fancy test mode!  TestModeControl(true) puts the swerve modules in
