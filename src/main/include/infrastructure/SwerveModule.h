bc8b03547db848437638476d804324a9de98731e
Scott <114890628+skoot1331@users.noreply.github.com>
2024-02-06 19:57:14 -0500

    Revert "Update SwerveModule.h"
    This reverts commit 5f0733ddea946e77e8fd4cae6a4cc1acebb3498b.

--- src/main/include/infrastructure/SwerveModule.h
+++ src/main/include/infrastructure/SwerveModule.h
@@ -177,7 +177,7 @@ public:
 
   // Determine if commanded turning position has been achieved, to within
   // specified tolerance.
-  bool CheckTurningPosition(const units::angle::degree_t tolerance = 10_deg) noexcept;
+  bool CheckTurningPosition(const units::angle::degree_t tolerance = 2.5_deg) noexcept;
 
   // Drive is normally oriented around velocity, but distance enables odometry,
   // simple dead reckoning, etc.  Possibly useful for autonomous driving.
