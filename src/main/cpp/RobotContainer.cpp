f1910a1c66b4438beb75256f14c45c0bc16d9e88
James2028s <156536948+James2028s@users.noreply.github.com>
2024-02-27 20:08:29 -0500

    change some auto code

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -72,18 +72,20 @@ void RobotContainer::AutonomousExit() noexcept {}
 
 std::optional<frc2::CommandPtr> RobotContainer::GetAutonomousCommand() noexcept
 {
-  frc::TrajectoryConfig trajectoryConfig{4.0_mps, 2.0_mps_sq};
+  frc::TrajectoryConfig trajectoryConfig{8.0_mps, 2.0_mps_sq};
   frc::SwerveDriveKinematics<4> kinematics{m_driveSubsystem.kDriveKinematics};
 
   trajectoryConfig.SetKinematics(kinematics);
 
   // TODO: Update trajectory path to our autonomous path
   frc::Trajectory trajectory = frc::TrajectoryGenerator::GenerateTrajectory(
-      {frc::Pose2d{},
+      {/*frc::Pose2d{},*/
        frc::Pose2d{1.0_m, 0.0_m, frc::Rotation2d{}}},
-      trajectoryConfig);
-
-  return TrajectoryAuto::TrajectoryAutoCommandFactory(&m_driveSubsystem, "Test Trajectory", trajectory);
+       
+    trajectoryConfig);
+  
+  return ShootCommands(&m_shooterSubsystem).ToPtr()
+  .AndThen(TrajectoryAuto::TrajectoryAutoCommandFactory(&m_driveSubsystem, "test trajectory", trajectory));
 }
 #pragma endregion
 
